54bcad117a92ddfc5fa010fd657a0e6655dd37a9
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-09-01 17:12:33 +0300

    Добавляем Ид Семьи

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,18 @@
 public class Person {
     public String fullName;
 
-    public Person(String fullName) {
+    public Integer idFamily;
+
+    public Person(String fullName,Integer idFamily ) {
         this.fullName = fullName;
+        this.idFamily = idFamily;
     }
 
     public String getFullName() {
         return fullName;
+    }
+    public Integer getIdFamily() {
+        return idFamily;
     }
 
 }
